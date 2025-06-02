e0caa83ee39cbaa966d6642510af2907131ef913
Dan Sniderman <168007543+dansniderman@users.noreply.github.com>
2025-06-02 12:18:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  
-server: {
+  server: {
     allowedHosts: [
       'dps-gha-workshop2-a6hedab9b8dmdvh4.centralus-01.azurewebsites.net'
     ]
-  }
-
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
 })
